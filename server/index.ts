c5ec02e70e4f9a13b1927491014a3d6a9eaebb5c
icaroweasley <icaroweasley@gmail.com>
2026-05-29 20:29:16 -0400

    Increase Express body limit to 50mb to allow image uploads

--- server/index.ts
+++ server/index.ts
@@ -18,7 +18,8 @@ const mpClient = new MercadoPagoConfig({
 });
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Middleware to verify JWT
 const authenticateToken = (req: any, res: any, next: any) => {
